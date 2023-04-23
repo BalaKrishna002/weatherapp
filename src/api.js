e1b05c0ae058b081dc53990bd87dbbaff9c21638
Bala Krishna <balumakineni8008@gmail.com>
2023-04-23 15:16:44 +0530

    adding all files

---
+++ src/api.js
@@ -0,0 +1,19 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b30758a36bmshca35f0b82ef5cfdp160393jsne9abf0d05e96',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "16bad640b08296593c24be4ac590ed0c";
+
+// fetch('/cities', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
